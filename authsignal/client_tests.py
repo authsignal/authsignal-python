5d5fcbaa4fbec3b3a4909602826d34526b40c0d4
Steven Clouston <stevenclouston@gmail.com>
2023-11-16 16:00:24 +1300

    Create client_tests.py

---
+++ authsignal/client_tests.py
@@ -0,0 +1,39 @@
+import unittest
+import responses
+
+import client
+
+class Test(unittest.TestCase):
+    def setUp(self):
+        self.authsignal_client = client.Client(api_key='<SECRET API KEY HERE>')
+
+    @responses.activate
+    def test_enroll_verified_authenticator(self):
+        payload = {
+            "authenticator": {
+            "userAuthenticatorId": "9b2cfd40-7df2-4658-852d-a0c3456e5a2e",
+            "authenticatorType": "OOB",
+            "isDefault": "true",
+            "phoneNumber": "+64270000000",
+            "createdAt": "2022-07-25T03:31:36.219Z",
+            "oobChannel": "SMS"
+            },
+            "recoveryCodes": ["xxxx"]
+        }
+
+        
+        responses.add(responses.POST, 'https://signal.authsignal.com/v1/users/1234/authenticators',
+                      json=payload, status=200)
+            
+        response = self.authsignal_client.enroll_verified_authenticator(
+                user_id="1234",
+                authenticator_payload={
+                    "oob_channel": "SMS",
+                    "phone_number": "+64270000000"
+                })
+
+        self.assertEqual(response["authenticator"]["userAuthenticatorId"],
+                             "9b2cfd40-7df2-4658-852d-a0c3456e5a2e")
+
+if __name__ == "__main__":
+    unittest.main()

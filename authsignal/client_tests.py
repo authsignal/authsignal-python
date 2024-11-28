331bebc6db06f469d604cdea45459879a154638f
Jono Templeton <littlejono@icloud.com>
2024-11-29 10:44:41 +1300

    AUT-2461: updating tests

--- authsignal/client_tests.py
+++ authsignal/client_tests.py
@@ -1,238 +1,237 @@
+import os
 import unittest
-import responses
-import jwt
-import time
+from .client import AuthsignalClient, ApiException
 
-import client
 
-base_url = "https://api.authsignal.com/v1"
-
-class Test(unittest.TestCase):
-    def setUp(self):
-        self.authsignal_client = client.Client(api_key='SECRET')
-
-    @responses.activate
-    def test_get_user(self):
-        responses.add(responses.GET, f"{base_url}/users/1234",
-                json={"isEnrolled": False, "email": "test@gmail.com", "phoneNumber": "1234567"}, status=200)
-
-        response = self.authsignal_client.get_user(user_id="1234")
-
-        self.assertEqual(response["is_enrolled"], False)
-        self.assertEqual(response["email"], "test@gmail.com")
-        self.assertEqual(response["phone_number"], "1234567")
-
-    @responses.activate
-    def test_enroll_verified_authenticator(self):
-        payload = {
-            "authenticator": {
-            "userAuthenticatorId": "9b2cfd40-7df2-4658-852d-a0c3456e5a2e",
-            "authenticatorType": "OOB",
-            "isDefault": "true",
-            "phoneNumber": "+64270000000",
-            "createdAt": "2022-07-25T03:31:36.219Z",
-            "oobChannel": "SMS"
-            },
-            "recoveryCodes": ["xxxx"]
+class TestAuthsignalClient(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.test_config = {
+            "api_secret_key": os.getenv("AUTHSIGNAL_API_SECRET"),
+            "api_url": os.getenv("AUTHSIGNAL_API_URL"),
         }
 
-        
-        responses.add(responses.POST, f"{base_url}/users/1234/authenticators",
-                      json=payload, status=200)
-            
-        response = self.authsignal_client.enroll_verified_authenticator(
-                user_id="1234",
-                authenticator_payload={
-                    "oob_channel": "SMS",
-                    "phone_number": "+64270000000"
-                })
-
-        self.assertEqual(response["authenticator"]["user_authenticator_id"],
-                             "9b2cfd40-7df2-4658-852d-a0c3456e5a2e")
-
-    @responses.activate
-    def test_track(self):
-        responses.add(responses.POST, f"{base_url}/users/1234/actions/signIn",
-                      json={"state": "ALLOW", "idempotencyKey": "f7f6ff4c-600f-4d61-99a2-b1157fe43777", "ruleIds": []},
-                      status=200)
-
-        response = self.authsignal_client.track(
-            user_id="1234",
-            action="signIn",
-            payload={
-                "it_could_be_a_bool": True,
-                "it_could_be_a_string": "test",
-                "it_could_be_a_number": 400.00
-            }
+    def test_actions(self):
+        client = AuthsignalClient(
+            api_secret_key=self.test_config["api_secret_key"],
+            api_url=self.test_config["api_url"],
         )
 
-        self.assertEqual(response["state"], "ALLOW")
-        self.assertEqual(response["idempotency_key"], "f7f6ff4c-600f-4d61-99a2-b1157fe43777")
-
-    @responses.activate
-    def test_get_action(self):
-        responses.add(responses.GET, f"{base_url}/users/1234/actions/signIn/15cac140-f639-48c5-92db-835ec8d3d144",
-                      json={"state": "ALLOW", "ruleIds": [], "stateUpdatedAt": "2022-07-25T03:19:00.316Z", "createdAt": "2022-07-25T03:19:00.316Z"},
-                      status=200)
-
-        response = self.authsignal_client.get_action(
-            user_id="1234",
-            action="signIn",
-            idempotency_key="15cac140-f639-48c5-92db-835ec8d3d144",
-        )
-
-        self.assertEqual(response["state"], "ALLOW")
-        self.assertEqual(response["state_updated_at"], "2022-07-25T03:19:00.316Z")
-
-    @responses.activate
-    def test_delete_user(self):
-        responses.add(responses.DELETE, f"{base_url}/users/1234",
-                      json={"success": True}, status=200)
-
-        response = self.authsignal_client.delete_user(user_id="1234")
-
-        self.assertEqual(response["success"], True)
-        
-    @responses.activate
-    def test_update_user(self):
-        user_id = "1234"
-        data = {"email": "newemail@gmail.com"}
-        expected_response = {"email": "newemail@gmail.com"}
-
-        responses.add(responses.POST, f"{base_url}/users/{user_id}",
-                      json=expected_response, status=200)
-
-        response = self.authsignal_client.update_user(user_id=user_id, data=data)
-
-        self.assertEqual(response["email"], "newemail@gmail.com")
-
-class ValidateChallenge(unittest.TestCase):
-    def setUp(self):
-        self.api_key='SECRET'
-
-        self.authsignal_client = client.Client(api_key=self.api_key)
-
-        self.payload = {
-            "iat": int(time.time()),
-            "sub": "legitimate_user_id",
-            "exp": int(time.time()) + 10 * 60,
-            "iss": "test",
-            "scope": "read:authenticators add:authenticators update:authenticators remove:authenticators",
-            "other": {
-                "tenantId": "555159e4-adc3-454b-82b1-b55a2783f712",
-                "publishableKey": "2fff14a6600b7a58170793109c78b876",
-                "userId": "legitimate_user_id",
-                "action": "alwaysChallenge",
-                "idempotencyKey": "a682af7d-c929-4c29-9c2a-71e69ab5c603"
-            }
+        track_attributes = {
+            "redirect_url": "http://localhost:3000",
+            "redirect_to_settings": True,
+            "email": "not-a-real-email@authsignal.com",
+            "phone_number": "1234567890",
+            "ip_address": "127.0.0.1",
+            "user_agent": "Authsignal-Python-SDK-Tests/1.0",
+            "device_id": "device123",
+            "custom": {"hello": "world"},
         }
 
-        self.jwt_token = jwt.encode(self.payload, self.api_key, algorithm='HS256')
+        track_response = client.track(
+            user_id="user123",
+            action="python-sdk-test",
+            attributes=track_attributes,
+        )
+        self.assertEqual(track_response["state"], "CHALLENGE_REQUIRED")
+        self.assertTrue(track_response.get("idempotency_key"))
+        self.assertTrue(track_response.get("is_enrolled", False))
+        self.assertTrue(track_response.get("url"))
+        self.assertTrue(track_response.get("token"))
+        self.assertTrue(len(track_response.get("allowed_verification_methods", [])) > 0)
 
-    @responses.activate
-    def test_it_returns_success_if_user_id_is_correct(self):
-        responses.add(responses.POST, f"{base_url}/validate",
-            json={
-                'isValid': True, 
-                'state': 'CHALLENGE_SUCCEEDED', 
-                'stateUpdatedAt': '2024-07-11T22:03:39.037Z', 
-                'userId': 'legitimate_user_id', 
-                'action': 'signin', 
-                'idempotencyKey': 'f2a0275e-bdbb-464a-8398-13c60c98097c'
+        get_action_response = client.get_action(
+            user_id="user123",
+            action="python-sdk-test",
+            idempotency_key=track_response["idempotency_key"],
+        )
+        self.assertEqual(get_action_response["state"], "CHALLENGE_REQUIRED")
+        self.assertIsNotNone(get_action_response.get("output"))
+
+        update_action_response = client.update_action(
+            user_id="user123",
+            action="python-sdk-test",
+            idempotency_key=track_response["idempotency_key"],
+            attributes={"state": "BLOCK"},
+        )
+        self.assertEqual(update_action_response["state"], "BLOCK")
+
+    def test_validate_challenge(self):
+        client = AuthsignalClient(
+            api_secret_key=self.test_config["api_secret_key"],
+            api_url=self.test_config["api_url"],
+        )
+
+        track_response = client.track(
+            user_id="user123",
+            action="python-sdk-test",
+            attributes={
+                "redirect_url": "http://localhost:3000",
+                "redirect_to_settings": True,
+                "email": "not-a-real-email@authsignal.com",
+                "phone_number": "1234567890",
+                "ip_address": "127.0.0.1",
+                "user_agent": "Authsignal-Python-SDK-Tests/1.0",
+                "device_id": "device123",
             },
-            status=200
         )
 
-        response = self.authsignal_client.validate_challenge(user_id="legitimate_user_id", token=self.jwt_token)
-
-        self.assertEqual(response["user_id"], "legitimate_user_id")
-        self.assertEqual(response["state"], "CHALLENGE_SUCCEEDED")
-        self.assertTrue(response["is_valid"])
-
-    @responses.activate
-    def test_delete_authenticator(self):
-        self.authsignal_client = client.Client(api_key='test_api_key')
-        user_id = 'test_user'
-        user_authenticator_id = 'test_authenticator'
-
-        expected_url = f'{self.authsignal_client.url}/v1/users/{user_id}/authenticators/{user_authenticator_id}'
-        
-        responses.add(responses.DELETE, expected_url, json={"success": True}, status=200)
-
-        response = self.authsignal_client.delete_authenticator(user_id, user_authenticator_id)
-
-        self.assertEqual(response["success"], True)
-        self.assertEqual(len(responses.calls), 1)
-        self.assertEqual(responses.calls[0].request.url, expected_url)
-        self.assertEqual(responses.calls[0].response.status_code, 200)
-
-    @responses.activate
-    def test_validate_challenge_returns_success_false_if_user_id_is_incorrect(self):
-        responses.add(responses.POST, f"{base_url}/validate",
-            json={'isValid': False, 'error': 'User is invalid.'},
-            status=200
-        )
-
-        response = self.authsignal_client.validate_challenge(user_id="spoofed_id", token=self.jwt_token)
-
-        self.assertFalse(response['is_valid'])
-        self.assertEqual(response.get("error"), "User is invalid.")
-
-    @responses.activate
-    def test_validate_challenge_returns_isValid_false_if_action_is_incorrect(self):
-        responses.add(responses.POST, f"{base_url}/validate",
-            json={
-                'isValid': False, 
-                'error': 'Action is invalid.',
+        validate_response = client.validate_challenge(
+            attributes={
+                "token": track_response["token"],
             },
-            status=200
+        )
+        self.assertIsNotNone(validate_response.get("is_valid"))
+        self.assertEqual(validate_response["state"], "CHALLENGE_REQUIRED")
+        self.assertEqual(validate_response["user_id"], "user123")
+        self.assertEqual(validate_response["action"], "python-sdk-test")
+        self.assertTrue(validate_response.get("idempotency_key"))
+
+    def test_authenticators(self):
+        client = AuthsignalClient(
+            api_secret_key=self.test_config["api_secret_key"],
+            api_url=self.test_config["api_url"],
         )
 
-        response = self.authsignal_client.validate_challenge(action="malicious_action_id", token=self.jwt_token)
-
-        # self.assertEqual(response["error"], "CHALLENGE_SUCCEEDED")
-        self.assertFalse(response["is_valid"])
-
-    @responses.activate
-    def test_validate_challenge_returns_success_true_if_no_user_id_is_provided(self):
-        responses.add(responses.POST, f"{base_url}/validate",
-            json={
-                'isValid': True, 
-                'state': 'CHALLENGE_SUCCEEDED', 
-                'stateUpdatedAt': '2024-07-11T22:39:23.613Z', 
-                'userId': 'legitimate_user_id', 
-                'action': 'signin',
-                'idempotencyKey': '6d09db21-1aa9-4b7f-826f-dbc6a0af79eb', 
-                'verificationMethod': 'EMAIL_MAGIC_LINK'
+        enroll_response = client.enroll_verified_authenticator(
+            user_id="user12345",
+            attributes={
+                "verificationMethod": "EMAIL_OTP",
+                "email": "not-a-real-email@authsignal.com",
             },
-            status=200
+        )
+        self.assertTrue(enroll_response["authenticator"]["user_authenticator_id"])
+        self.assertEqual(
+            enroll_response["authenticator"]["verification_method"],
+            "EMAIL_OTP",
+        )
+        self.assertEqual(
+            enroll_response["authenticator"]["email"],
+            "not-a-real-email@authsignal.com",
+        )
+        self.assertEqual(enroll_response["authenticator"]["user_id"], "user12345")
+
+        get_auth_response = client.get_authenticators(user_id="user12345")
+        self.assertTrue(len(get_auth_response) > 0)
+
+        authenticator = get_auth_response[0]
+        self.assertTrue(authenticator["user_authenticator_id"])
+        self.assertEqual(
+            authenticator["verification_method"],
+            "EMAIL_OTP",
+        )
+        self.assertEqual(authenticator["email"], "not-a-real-email@authsignal.com")
+
+        client.delete_authenticator(
+            user_id="user12345",
+            user_authenticator_id=authenticator["user_authenticator_id"],
         )
 
-        response = self.authsignal_client.validate_challenge(token=self.jwt_token)
+        get_auth_response_after_delete = client.get_authenticators(user_id="user12345")
+        for auth in get_auth_response_after_delete:
+            self.assertNotEqual(
+                auth["user_authenticator_id"], authenticator["user_authenticator_id"]
+            )
 
-        self.assertEqual(response["user_id"], "legitimate_user_id")
-        self.assertEqual(response["state"], "CHALLENGE_SUCCEEDED")
-        self.assertTrue(response["is_valid"])
+    def test_new_client(self):
+        client = AuthsignalClient("secret", "https://api.authsignal.com")
+        self.assertEqual(client.api_secret_key, "secret")
+        self.assertEqual(client.api_url, "https://api.authsignal.com")
+        self.assertIsNotNone(client.session)
 
-    @responses.activate
-    def test_action_code_is_omitted_from_validate_challenge_response(self):
-        responses.add(responses.POST, f"{base_url}/validate",
-            json={
-                'isValid': True, 
-                'state': 'CHALLENGE_SUCCEEDED', 
-                'stateUpdatedAt': '2024-07-11T22:39:23.613Z', 
-                'userId': 'legitimate_user_id', 
-                'action': 'signin', 
-                'actionCode': 'signin',
-                'idempotencyKey': '6d09db21-1aa9-4b7f-826f-dbc6a0af79eb', 
-                'verificationMethod': 'EMAIL_MAGIC_LINK'
+    def test_new_authsignal_api_error(self):
+        error_code = "bad_request"
+        error_description = "An error occurred"
+        status_code = 400
+
+        api_error = ApiException(
+            error_code=error_code,
+            error_description=error_description,
+            status_code=status_code,
+        )
+
+        self.assertIsNotNone(api_error)
+        self.assertEqual(api_error.error_code, error_code)
+        self.assertEqual(api_error.error_description, error_description)
+        self.assertEqual(api_error.status_code, status_code)
+
+    def test_authsignal_api_error_message(self):
+        status_code = 404
+        error_description = "Not Found"
+
+        api_error = ApiException(
+            error_code="ERR404",
+            error_description=error_description,
+            status_code=status_code,
+        )
+        expected_error_message = "AuthsignalException: 404 - Not Found"
+        self.assertEqual(str(api_error), expected_error_message)
+
+    def test_users(self):
+        client = AuthsignalClient(
+            api_secret_key=self.test_config["api_secret_key"],
+            api_url=self.test_config["api_url"],
+        )
+
+        update_user_response = client.update_user(
+            user_id="a-new-user",
+            attributes={
+                "phoneNumber": "9876543210",
+                "displayName": "A New User",
             },
-            status=200
         )
 
-        response = self.authsignal_client.validate_challenge(token=self.jwt_token)
+        self.assertEqual(
+            update_user_response["phone_number"],
+            "9876543210",
+        )
 
-        self.assertNotIn("action_code", response)
+        self.assertEqual(
+            update_user_response["display_name"],
+            "A New User",
+        )
+
+        get_user_response = client.get_user(user_id="a-new-user")
+        self.assertEqual(
+            get_user_response["phone_number"],
+            "9876543210",
+        )
+        self.assertEqual(
+            get_user_response["display_name"],
+            "A New User",
+        )
+
+        client.delete_user(user_id="a-new-user")
+
+        get_user_response = client.get_user(user_id="a-new-user")
+        self.assertIsNotNone(get_user_response.get("is_enrolled"))
+        self.assertFalse(get_user_response["is_enrolled"])
+
+    def test_get_action_with_bad_secret(self):
+        client = AuthsignalClient(
+            api_secret_key="bad-secret",
+            api_url=self.test_config["api_url"],
+        )
+
+        with self.assertRaises(ApiException) as cm:
+            client.get_action(
+                user_id="test-user",
+                action="python-sdk-test",
+                idempotency_key="test-key",
+            )
+
+        api_error = cm.exception
+        self.assertEqual(api_error.status_code, 401)
+        self.assertEqual(
+            api_error.error_description,
+            "The request is unauthorized. Check that your API key and region base URL are correctly configured.",
+        )
+        self.assertEqual(api_error.error_code, "unauthorized")
+        self.assertEqual(
+            str(api_error),
+            "AuthsignalException: 401 - The request is unauthorized. Check that your API key and region base URL are correctly configured.",
+        )
+
 
 if __name__ == "__main__":
     unittest.main()
